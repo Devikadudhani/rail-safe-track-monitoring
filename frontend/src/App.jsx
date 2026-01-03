bb49ddbc724410735f6b760183655d43104b4388
dimpalagarwal <alwardimpal12@gmail.com>
2026-01-03 19:55:30 +0530

    fixed routing of login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import TrackMonitor from "./pages/TrackMonitor";
 import TrafficControlPage from "./pages/TrafficControlPage";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/monitoring" element={<TrackMonitor />} />
       <Route path="/traffic-control" element={<TrafficControlPage />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
